f59bc6bae1b73ef5ad2176f831c452875b3d5ce7
Nckflannery <Nckflannery@users.noreply.github.com>
2020-04-15 12:06:42 -0700

    Day3 Lecture Notes

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,3 +1,5 @@
+import random
+
 class User:
     def __init__(self, name):
         self.name = name
@@ -47,6 +49,24 @@ class SocialGraph:
         # Add users
 
         # Create friendships
+        for i in range(0, num_users):
+            self.add_user(f'Fred{i + 1}')
+
+        # Generate all friendship combinations
+        possible_friendships = []
+        
+        # Avoid duplicates by making sure first number is smaller
+        for user_id in self.users:
+            for friend_id in range(user_id + 1, self.last_id + 1):
+                possible_friendships.append((user_id, friend_id))
+
+        # Shuffle all possible friendships
+        random.shuffle(possible_friendships)
+
+        # Create for first X pairs
+        for i in range(num_users * avg_friendships // 2):
+            friendship = possible_friendships[i]
+            self.add_friendship(friendship[0], friendship[1])
 
     def get_all_social_paths(self, user_id):
         """
